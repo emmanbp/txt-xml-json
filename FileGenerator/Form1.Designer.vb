f994dd1f8ee6db6f54f272191f888e545de37ca5
EmmanPanopio <emmanuelpanopio10@gmail.com>
2020-02-22 00:23:24 +0800

    first commit

---
+++ FileGenerator/Form1.Designer.vb
@@ -0,0 +1,162 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Address = New System.Windows.Forms.TextBox()
+        Me.userAddress = New System.Windows.Forms.Label()
+        Me.userName = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(164, 111)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(336, 26)
+        Me.TextBox1.TabIndex = 17
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Teio", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.FromArgb(CType(CType(78, Byte), Integer), CType(CType(184, Byte), Integer), CType(CType(206, Byte), Integer))
+        Me.Label3.Location = New System.Drawing.Point(225, 31)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(143, 30)
+        Me.Label3.TabIndex = 16
+        Me.Label3.Text = "Hello!"
+        '
+        'Address
+        '
+        Me.Address.Location = New System.Drawing.Point(164, 163)
+        Me.Address.Name = "Address"
+        Me.Address.Size = New System.Drawing.Size(336, 26)
+        Me.Address.TabIndex = 10
+        '
+        'userAddress
+        '
+        Me.userAddress.AutoSize = True
+        Me.userAddress.Font = New System.Drawing.Font("Stencil", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.userAddress.ForeColor = System.Drawing.Color.White
+        Me.userAddress.Location = New System.Drawing.Point(28, 163)
+        Me.userAddress.Name = "userAddress"
+        Me.userAddress.Size = New System.Drawing.Size(117, 29)
+        Me.userAddress.TabIndex = 9
+        Me.userAddress.Text = "Address"
+        '
+        'userName
+        '
+        Me.userName.AutoSize = True
+        Me.userName.Font = New System.Drawing.Font("Stencil", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.userName.ForeColor = System.Drawing.Color.White
+        Me.userName.Location = New System.Drawing.Point(68, 111)
+        Me.userName.Name = "userName"
+        Me.userName.Size = New System.Drawing.Size(77, 29)
+        Me.userName.TabIndex = 7
+        Me.userName.Text = "Name"
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(34, Byte), Integer), CType(CType(36, Byte), Integer), CType(CType(49, Byte), Integer))
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel1.Controls.Add(Me.TextBox1)
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Controls.Add(Me.Button3)
+        Me.Panel1.Controls.Add(Me.Button2)
+        Me.Panel1.Controls.Add(Me.Button1)
+        Me.Panel1.Controls.Add(Me.Address)
+        Me.Panel1.Controls.Add(Me.userAddress)
+        Me.Panel1.Controls.Add(Me.userName)
+        Me.Panel1.Location = New System.Drawing.Point(3, 2)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(591, 452)
+        Me.Panel1.TabIndex = 1
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.Color.FromArgb(CType(CType(78, Byte), Integer), CType(CType(184, Byte), Integer), CType(CType(206, Byte), Integer))
+        Me.Button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button3.Font = New System.Drawing.Font("Zealot Expanded", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(380, 297)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(101, 44)
+        Me.Button3.TabIndex = 13
+        Me.Button3.Text = "JSON"
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.FromArgb(CType(CType(78, Byte), Integer), CType(CType(184, Byte), Integer), CType(CType(206, Byte), Integer))
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.Font = New System.Drawing.Font("Zealot Expanded", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(238, 297)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(91, 44)
+        Me.Button2.TabIndex = 12
+        Me.Button2.Text = "XML"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.FromArgb(CType(CType(78, Byte), Integer), CType(CType(184, Byte), Integer), CType(CType(206, Byte), Integer))
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Font = New System.Drawing.Font("Zealot Expanded", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(103, 297)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(82, 44)
+        Me.Button1.TabIndex = 11
+        Me.Button1.Text = "TXT"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(578, 444)
+        Me.Controls.Add(Me.Panel1)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Generate File"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Address As TextBox
+    Friend WithEvents userAddress As Label
+    Friend WithEvents userName As Label
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+End Class
---
+++ FileGenerator/Form1.vb
@@ -0,0 +1,106 @@
+﻿Imports System.IO
+Imports System.Text
+Imports System.Xml
+Imports Newtonsoft.Json
+
+Public Class Form1
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub Address_TextChanged(sender As Object, e As EventArgs) Handles Address.TextChanged
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        createText()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim timeStamp As DateTime = DateTime.Now
+        Dim current As String = timeStamp.ToString("MM-yyyy-HH-mm-ss")
+        Dim username As String = TextBox1.Text
+        Dim useraddress As String = Address.Text
+
+        Dim xmlpath As String = "c:\Generated Files\" + current + ".xml"
+
+        Dim writer As New XmlTextWriter(xmlpath, System.Text.Encoding.UTF8)
+        writer.WriteStartDocument(True)
+        writer.Formatting = System.Xml.Formatting.Indented
+        writer.Indentation = 2
+        writer.WriteStartElement("UserInfo")
+        createElement(username, useraddress, writer)
+        writer.WriteEndElement()
+        writer.WriteEndDocument()
+        writer.Close()
+
+        TextBox1.Text = ""
+        Address.Text = ""
+
+        MessageBox.Show("Check c:\Generated Files\", ".XML FILE CREATED", MessageBoxButtons.OKCancel)
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim timeStamp As DateTime = DateTime.Now
+        Dim current As String = timeStamp.ToString("MM-yyyy-HH-mm-ss")
+        Dim username As String = TextBox1.Text
+        Dim useraddress As String = Address.Text
+
+        Dim sb As New StringBuilder()
+        Dim sw As New StringWriter(sb)
+        Using writer As JsonWriter = New JsonTextWriter(sw)
+            writer.Formatting = Newtonsoft.Json.Formatting.Indented
+            writer.WriteStartObject()
+            writer.WritePropertyName("Name")
+            writer.WriteValue(username)
+            writer.WritePropertyName("Address")
+            writer.WriteValue(useraddress)
+            writer.WriteEndObject()
+        End Using
+
+        Dim path As String = "c:\Generated Files\" + current + ".json"
+
+        Dim fs As FileStream = File.Create(path)
+
+        Dim info As Byte() = New UTF8Encoding(True).GetBytes(sw.ToString())
+        fs.Write(info, 0, info.Length)
+        fs.Close()
+
+        TextBox1.Text = ""
+        Address.Text = ""
+
+        MessageBox.Show("Check c:\Generated Files\", ".JSON FILE CREATED", MessageBoxButtons.OKCancel)
+    End Sub
+
+    Private Sub createText()
+        Dim timeStamp As DateTime = DateTime.Now
+        Dim current As String = timeStamp.ToString("MM-yyyy-HH-mm-ss")
+        Dim username As String = TextBox1.Text
+        Dim useraddress As String = Address.Text
+
+        Dim path As String = "c:\Generated Files\" + current + ".txt"
+
+        ' Create or overwrite the file.
+        Dim fs As FileStream = File.Create(path)
+
+        ' Add text to the file.
+        Dim info As Byte() = New UTF8Encoding(True).GetBytes("Name: " + username + Environment.NewLine + "Address: " + useraddress + "")
+        fs.Write(info, 0, info.Length)
+        fs.Close()
+
+        TextBox1.Text = ""
+        Address.Text = ""
+
+        MessageBox.Show("Check c:\Generated Files\", ".TXT FILE CREATED", MessageBoxButtons.OKCancel)
+
+    End Sub
+    Private Sub createElement(ByVal username As String, ByVal address As String, ByVal writer As XmlTextWriter)
+        writer.WriteStartElement("Name")
+        writer.WriteString(username)
+        writer.WriteEndElement()
+        writer.WriteStartElement("Address")
+        writer.WriteString(address)
+        writer.WriteEndElement()
+    End Sub
+End Class
